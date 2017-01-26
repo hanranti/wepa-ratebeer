918c9cbf1fa0328b97768482d70e6eccb13b9c2f
hanranti <hanranti@cs.helsinki.fi>
2017-01-26 18:01:40 +0200

    tehtava4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        average = 0
+        self.ratings.each do |rating|
+            average += rating.score
+        end
+        average /= self.ratings.count
+    end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,6 @@ class Rating < ActiveRecord::Base
     belongs_to :beer
 
     def to_s
-        return beer.name.to_s + " " + score.to_s 
+        beer.name.to_s + " " + score.to_s 
     end
 end
