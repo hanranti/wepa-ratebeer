a9cb815b197f2bd46913818bc7408ffabe1b182e
hanranti <hanranti@cs.helsinki.fi>
2017-01-27 14:31:43 +0200

    tehtava5

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,10 +3,23 @@ class Beer < ActiveRecord::Base
     has_many :ratings
 
     def average_rating
-        average = 0
-        self.ratings.each do |rating|
-            average += rating.score
+#ei toimi
+#        average = ratings.inject { |rating, n| rating.score + n }
+#        average /= ratings.count
+
+        scores = Array.new;
+        ratings.each do |rating| 
+            scores << rating.score
         end
-        average /= self.ratings.count
+        average = scores.inject {|score, n| score + n}
+        average /= scores.count
+
+#        average = 0;
+#        ratings.each do |rating|
+#            average += rating.score
+#        end
+#        average /= ratings.count
+
+#        ratings.average(:score)
     end
 end
