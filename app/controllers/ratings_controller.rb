85543951c9d95c0335a277bfa5d7813e29131bfe
hanranti <hanranti@cs.helsinki.fi>
2017-01-28 18:05:33 +0200

    tehtava11

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -15,4 +15,10 @@ class RatingsController < ApplicationController
         end
         redirect_to ratings_path
     end
+
+    def destroy
+        rating = Rating.find(params[:id])
+        rating.delete
+        redirect_to ratings_path
+    end
 end
